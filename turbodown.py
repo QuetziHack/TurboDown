d1d5b2bc6c6ee09c05ebc0c2332fec3ff650231d
QuetziHack <quetzalion.de.fuego@gmail.com>
2023-01-26 23:35:16 -0600

    Se implementaon las funcionalidades de mp3 y mp4 y se creo el archivo del diagrama de clases

--- turbodown.py
+++ turbodown.py
@@ -1,4 +1,36 @@
 import youtube_dl
+
+class Video:
+    def __init__(self,url) -> None: 
+        self.url = url
+        
+    def generarnombre(self,codec):
+        video_info = youtube_dl.YoutubeDL().extract_info(url = self.url,download=False)
+        self.filename = f"{video_info['title']}.{codec}"
+
+    def setMp3(self):
+        self.options = {
+            'format': 'bestaudio/best',
+            'keepvideo': False,
+            'outtmpl': '%(title)s.%(ext)s',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+            }]
+        }
+        self.generarnombre('mp3')
+
+    def setMp4(self):
+        options = {
+            'format': 'bestvideo[height<=1080]+bestaudio/best[height<=1080]',
+            'outtmpl': '%(title)s.%(ext)s',
+        }
+        self.generarnombre('mp4')
+
+    def descargar(self):
+        with youtube_dl.YoutubeDL(self.options) as ydl:
+            ydl.download([self.url])
+
 def download_ytvid_as_mp3():
     video_url = input("enter url of youtube video:")
     video_info = youtube_dl.YoutubeDL().extract_info(url = video_url,download=False)
@@ -13,5 +45,14 @@ def download_ytvid_as_mp3():
         ydl.download([video_info['webpage_url']])
 
     print("Download complete... {}".format(filename))
-download_ytvid_as_mp3()
 
+
+#download_ytvid_as_mp3()
+
+def main():
+    url = input ("Ingresa la url del video, xfa: ")
+    videoyt = Video(url)
+    videoyt.setMp3()
+    videoyt.descargar()
+
+main()
--- ytd.py
+++ ytd.py
@@ -5,8 +5,7 @@ class Video:
 	def __init__(self,url, codec="mp3"):
 		self.url=url
 		self.codec=codec
-		self.video=pt.YouTube(input("Ingresa la url del video: ")
+		self.video=pt.YouTube(input("Ingresa la url del video: "))
 
-	def descarga_mp3(self):
-		mp3 = self.video.streams.
+	
 
